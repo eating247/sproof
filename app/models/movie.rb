40781e14d541d5f6173c107af2cbcf83a7cb331e
tingting <ttwang.303@gmail.com>
2017-06-25 14:08:36 -0400

    Expand search results to include partial matches

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,10 @@
 class Movie < ApplicationRecord
   include PgSearch
-  pg_search_scope :search, :against => [:title, :storyline]
+  pg_search_scope :search,
+                  :against => [:title, :storyline],
+                  :using => {
+                    :tsearch => { prefix: true }
+                  }
   belongs_to :genre
 
   validates_presence_of :title
