be3f66af07826c25ae054a3d3ee92ed6f25cba31
tingting <ttwang.303@gmail.com>
2017-06-25 13:44:12 -0400

    Expand search scope to include storyline

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   include PgSearch
-  pg_search_scope :search, :against => [:title]
+  pg_search_scope :search, :against => [:title, :storyline]
   belongs_to :genre
 
   validates_presence_of :title
