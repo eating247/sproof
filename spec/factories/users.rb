94637ac4fc319834ddc0b860e4354a3d4d71c28b
tingting <ttwang.303@gmail.com>
2017-06-26 12:21:10 -0400

    Revise tests to account for authentication
    - Sign in before testing tasks that require auth
    - Add Log out link to nav

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    
+    email 'admin@admin.com'
+    password 'password'
   end
 end
--- spec/features/user_deletes_movies_spec.rb
+++ spec/features/user_deletes_movies_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 feature 'User deletes movie' do
   let!(:movie) { FactoryGirl.create(:movie) }
+  let(:user){ FactoryGirl.create(:user) }
 
   before do
+    sign_in(user)
     visit movie_path(movie)
   end
 
