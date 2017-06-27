b3d77845b87a444a07258c609220ad9790c5263b
tingting <ttwang.303@gmail.com>
2017-06-27 12:40:27 -0400

    Add controller tests and refactor
    - Configure test env for controller tests
    - Refactor display logic for release date on show view into helper

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+  def display_release_date(release)
+    if release
+      release.strftime("%b %d, %Y")
+    else
+      "no date given"
+    end
+  end
 end
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,4 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
+  let(:user) { FactoryGirl.create(:user) }
+
+  context 'guest access' do
+    describe "#index" do
+      it "renders the index template" do
+        get :index
+        response.should render_template :index
+      end
+    end
+
+    describe "#show" do
+      it "renders the show view" do
+        movie = FactoryGirl.create(:movie)
+        get :show, id: movie.id
+        response.should render_template :show
+      end
+    end
+  end
+
+  context 'signed-in user access' do
+    describe "#create" do
+
+      it "is successful when user is authenticated" do
+        sign_in(user)
+        post :create, movie: FactoryGirl.attributes_for(:movie)
+        expect(response).to redirect_to movies_path
+      end
+
+      it "renders form when title is blank" do
+        sign_in(user)
+        post :create, movie: { title: nil }
+        expect(response).to render_template :new
+      end
+
+    end
+  end
 end
