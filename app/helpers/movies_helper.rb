6721d8edfe40cbc56ead17870f54504c7b3b5f58
tingting <ttwang.303@gmail.com>
2017-06-30 15:39:40 -0400

    Improves empty results view for no search results

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -6,4 +6,13 @@ module MoviesHelper
       'no date given'
     end
   end
+
+  def display_search_message(movies_length)
+    if movies_length > 0
+      pluralize( movies_length, 'Search Result' )
+    else
+      "Sorry, your search did not return any results."
+    end
+  end
+
 end
