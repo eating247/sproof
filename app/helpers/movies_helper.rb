3aa2a94cc578dabdb063b8c5cae404fd8efaec9a
tingting <ttwang.303@gmail.com>
2017-06-30 16:25:59 -0400

    Remove redundant helper for search views

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -6,13 +6,4 @@ module MoviesHelper
       'no date given'
     end
   end
-
-  def display_search_message(movies_length)
-    if movies_length > 0
-      pluralize( movies_length, 'Search Result' )
-    else
-      "Sorry, your search did not return any results."
-    end
-  end
-
 end
